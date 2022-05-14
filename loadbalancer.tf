9ee7ca5969c5bb206ce466bb5ac38ea79b480abd
ปฐมพงษ์ วิมลเจริญ <patompong909@gmail.com>
2022-05-14 21:57:58 +0700

    draft lb

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,24 +1,8 @@
-resource "aws_lb" "webLB" {
-    name = "web-elb"
-    load_balancer_type = "application"
-    internal = false
-    subnets = module.vpc.PublicNets[*].id
-    security_groups = [aws_security_group.elb-sg.id]
-
-    tags = merge(local.common_tags, { Name = "${var.cName}-web-loadbalancer"})
+module "loadbalancer" {
+  
 }
 
-
-resource "aws_lb_listener" "lbListener" {
-  load_balancer_arn = aws_lb.webLB.arn
-  port = 80
-  protocol = "HTTP"
-
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.tgp.arn
-    
-  }
-
- tags = merge(local.common_tags, { Name = "${var.cName}-listener"})
+resource "aws_autoscaling_attachment" "asg_tgp" {
+  autoscaling_group_name = aws_autoscaling_group.asg.id#music asg
+  lb_target_group_arn    = aws_lb_target_group.test.arn
 }
