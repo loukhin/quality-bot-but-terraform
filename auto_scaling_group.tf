f8bd3b47b303fb8ad7bb7a05a0a3e992df169ebe
ปฐมพงษ์ วิมลเจริญ <patompong909@gmail.com>
2022-05-16 13:19:38 +0700

    delet unused module and add launch config

--- auto_scaling_group.tf
+++ auto_scaling_group.tf
@@ -1,6 +1,7 @@
 module "Manager1-asg" {
   source = "./modules/tom_asg"
-  launch_configuration = aws_launch_configuration.name
+  count = var.text_instance_count
+  launch_configuration = aws_launch_configuration.text_conf[count.index]
   min = var.text_group_size[0]
   max = var.text_group_size[2]
   desired = var.text_group_size[1]
@@ -9,25 +10,11 @@ module "Manager1-asg" {
   force_delete = true
   subnet_list = module.vpc.pubsubnet
   cName = var.pName
-}
-
-module "Manager2-asg" {
-  source = "./modules/tom_asg"
-  launch_configuration = aws_launch_configuration.name
-  min = var.text_group_size[0]
-  max = var.text_group_size[2]
-  desired = var.text_group_size[1]
-  health_check = 300
-  health_check_type = "ELB"
-  force_delete = true
-  subnet_list = module.vpc.pubsubnet
-  cName = var.pName
-
 }
 
 module "Music-asg" {
   source = "./modules/tom_asg"
-  launch_configuration = aws_launch_configuration.name
+  launch_configuration = aws_launch_configuration.voice_conf
   min = var.music_group_size[0]
   max = var.music_group_size[2]
   desired = var.music_group_size[1]
