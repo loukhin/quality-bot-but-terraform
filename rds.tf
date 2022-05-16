ea6d1f2ceba7828598f18d6b5af50cd180e25aaa
Patit Kaewjinda <riew10580@gmail.com>
2022-05-16 23:35:39 +0700

    Update rds.tf
    Remove some unused code

--- rds.tf
+++ rds.tf
@@ -1,11 +1,3 @@
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  token      = var.aws_session_token
-  region     = var.region
-}
-
 data "aws_ami" "aws-linux" {
   most_recent = true
   owners      = ["amazon"]
