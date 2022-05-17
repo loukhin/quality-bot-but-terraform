f916a8f80bb48c6947ca5a4f109bdcaffe8b0e60
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-05-17 23:52:49 +0700

    Replace placeholder

--- instance.tf
+++ instance.tf
@@ -31,6 +31,7 @@ resource "aws_launch_configuration" "voice_conf" {
   image_id      = data.aws_ami.aws-linux.id
   instance_type = var.instance_type
   key_name      = var.key_name
+  security_groups = [aws_security_group.quality-internal-web.id, aws_security_group.Quality-SG.id]
 
   user_data = <<-EOT
   #!/bin/bash
@@ -43,8 +44,7 @@ resource "aws_launch_configuration" "voice_conf" {
   DISCORD_CLIENT_ID=${var.discord_client_id}
   DISCORD_BOT_TOKEN=${var.discord_bot_token}
 
-  DATABASE_URL=postgresql://db_username:db_password@RDS_ENDPOINT_URL:5432/quality-bot
-  # ^ อันนี้ต้องรอ RDS
+  DATABASE_URL=postgresql://${module.rds.Quality_RDS.username}:${module.rds.Quality_RDS.password}@${module.rds.Quality_RDS.endpoint}/${module.rds.Quality_RDS.db_name}
 
   YOUTUBE_API_KEY=${var.youtube_api_key}
 
@@ -68,6 +68,7 @@ resource "aws_launch_configuration" "manager_conf" {
   image_id      = data.aws_ami.aws-linux.id
   instance_type = var.instance_type
   key_name      = var.key_name
+  security_groups = [aws_security_group.Quality-SG.id]
 
   user_data = <<-EOT
   #!/bin/bash
@@ -77,7 +78,7 @@ resource "aws_launch_configuration" "manager_conf" {
   NODE_ENV=development
   PORT=3000
 
-  ELB_URL=http://localhost:3000
+  ELB_URL=http://${module.loadbalancer.reqLB.dns_name}
   CONVERTER_FUNCTION_NAME=${aws_lambda_function.qb-file-converter.function_name}
   AWS_REGION=${var.region}
 
@@ -93,8 +94,7 @@ resource "aws_launch_configuration" "manager_conf" {
   SHARD_COUNT=${var.text_instance_count}
   SHARD_ID=${count.index}
 
-  DATABASE_URL=postgresql://db_username:db_password@RDS_ENDPOINT_URL:5432/quality-bot
-  # ^ อันนี้ต้องรอ RDS
+  DATABASE_URL=postgresql://${module.rds.Quality_RDS.username}:${module.rds.Quality_RDS.password}@${module.rds.Quality_RDS.endpoint}/${module.rds.Quality_RDS.db_name}
 
   # text | voice
   INSTANCE_TYPE=text
@@ -110,14 +110,15 @@ resource "aws_launch_configuration" "manager_conf" {
 }
 
 ##############################
-# FOR Transfer SQL File to RDS 
+# FOR Transfer SQL File to RDS
 ##############################
 
 resource "aws_instance" "RDS_file_transfer" {
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
   key_name               = var.key_name
-  
+  vpc_security_group_ids = [aws_security_group.Quality-SG.id]
+  subnet_id = module.vpc.pubsubnet[0].id
 
   tags = { Name = "${var.pName}-EC2-RDS"}
 
@@ -127,97 +128,17 @@ resource "aws_instance" "RDS_file_transfer" {
     user        = "ec2-user"
     private_key = file(var.private_key_path)
   }
-  
+
   provisioner "file" {
-    source      = var.sql_source
+    source      = "./modules/ProjectRDS/RDS_Files/quality-bot.sql"
     destination = "/tmp/quality-bot.sql"
   }
 
   provisioner "remote-exec" {
     inline = [
       "sudo yum install postgresql -y",
-      "PGPASSWORD=${module.rds.Quality_RDS.password} psql  -f /tmp/quality-bot.sql -h ${module.rds.Quality_RDS.address}  -p ${module.rds.Quality_RDS.port}  -U ${module.rds.Quality_RDS.username}  --dbname ${module.rds.Quality_RDS.db_name} "
+      "PGPASSWORD=${module.rds.Quality_RDS.password} psql  -f /tmp/quality-bot.sql -h ${module.rds.Quality_RDS.address}  -p ${module.rds.Quality_RDS.port}  -U ${module.rds.Quality_RDS.username}  --dbname ${module.rds.Quality_RDS.db_name}",
+      "sudo poweroff"
     ]
   }
 }
-
-
-###############
-# FOR TESTING #
-###############
-
-# resource "aws_instance" "voice_conf" {
-#   ami      = data.aws_ami.aws-linux.id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-#   vpc_security_group_ids = [aws_security_group.Quality-SG.id]
-#   subnet_id = module.vpc.subnet_list[0].id
-
-#   user_data = <<-EOT
-#   #!/bin/bash
-
-#   mkdir /opt/qualitybot
-#   echo "Tk9ERV9FTlY9cHJvZHVjdGlvbgpQT1JUPTgwCgpESVNDT1JEX0NMSUVOVF9JRD05MDY3NjQyOTE0NzcyMDkyMTgKRElTQ09SRF9CT1RfVE9LRU49T1RBMk56WTBNamt4TkRjM01qQTVNakU0LllZZFlEUS5tTWIzYXVEYkM2WDZWMEoxZTl2MktLQ25RaUEKCllPVVRVQkVfQVBJX0tFWT1BSXphU3lDQ2dYNVRqWlV3RzdaLUNrWER1MzdLMmw3Yjk4T3lMS3cKCiMgdGV4dCB8IHZvaWNlCklOU1RBTkNFX1RZUEU9dm9pY2UK" | base64 -d > /opt/qualitybot/.env
-#   # echo "DATABASE_URL=postgresql://db_username:db_password@$RDS_ENDPOINT_URL:5432/quality-bot" >> /opt/qualitybot/.env
-#   # ^ อันนี้ต้องรอ RDS
-
-#   sudo yum install -y docker
-#   sudo service docker start
-#   sudo usermod -a -G docker ec2-user
-#   sudo chkconfig docker on
-#   sudo docker pull ghcr.io/loukhin/quality-bot:latest
-#   sudo docker run -d --name quality-bot -p 80:80 --env-file /opt/qualitybot/.env ghcr.io/loukhin/quality-bot
-#   EOT
-# }
-
-# resource "aws_instance" "text_conf" {
-#   count = var.text_instance_count
-
-#   ami      = data.aws_ami.aws-linux.id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-#   vpc_security_group_ids = [aws_security_group.Quality-SG.id]
-#   subnet_id = module.vpc.subnet_list[count.index].id
-
-#   user_data = <<-EOT
-#   #!/bin/bash
-
-#   mkdir /opt/qualitybot
-#   cat > /opt/qualitybot/.env << EOF
-#   NODE_ENV=development
-#   PORT=3000
-
-#   ELB_URL=http://localhost:3000
-#   CONVERTER_FUNCTION_NAME=qb-converter
-#   AWS_REGION=${var.region}
-
-#   AWS_ACCESS_KEY=${var.aws_access_key}
-#   AWS_SECRET_KEY=${var.aws_secret_key}
-
-#   DISCORD_CLIENT_ID=${var.discord_client_id}
-#   DISCORD_BOT_TOKEN=${var.discord_bot_token}
-
-#   YOUTUBE_API_KEY=${var.youtube_api_key}
-
-#   SLASH_COMMAND_TEST_GUILD_LIST=834492419000500224, 713658288855842816, 901459733834252308, 610863827390824448
-#   SHARD_COUNT=${var.text_instance_count}
-#   SHARD_ID=${count.index}
-
-#   DATABASE_URL=postgresql://db_username:db_password@${data.aws_ami.aws-linux.id}:5432/quality-bot
-#   # ^ อันนี้ต้องรอ RDS
-
-#   # text | voice
-#   INSTANCE_TYPE=text
-#   EOF
-
-#   sudo yum install -y docker
-#   sudo service docker start
-#   sudo usermod -a -G docker ec2-user
-#   sudo chkconfig docker on
-#   sudo docker pull ghcr.io/loukhin/quality-bot:latest
-#   EOT
-
-#   tags = {
-#     "Name" = "text_conf_${count.index+1}"
-#   }
-# }
